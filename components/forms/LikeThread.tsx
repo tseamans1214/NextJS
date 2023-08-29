d7a9a395f37c5fdf64bd91e82ff957f14caef63e
Tommy Seamans <tlseamans1214@gmail.com>
2023-08-28 19:25:36 -0700

    Add LikeThread form to handle user likes

---
+++ components/forms/LikeThread.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { usePathname, useRouter } from "next/navigation";
+import { addUserLike } from "@/lib/actions/thread.actions";
+import Image from "next/image";
+
+interface Props {
+  threadId: string;
+  currentUserId: string;
+}
+
+function LikeThread({ threadId, currentUserId }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <Image 
+        src="/assets/heart-gray.svg" 
+        alt="heart" width="24"
+        height={24}
+        className="cursor-pointer object-contain"
+        onClick={async() =>{
+            await addUserLike(threadId, JSON.parse(currentUserId), pathname);
+            router.push(pathname);
+        }}
+    />
+  );
+}
+
+export default LikeThread;
